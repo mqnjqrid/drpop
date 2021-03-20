71a7716fe4dc9e99cd97d69a6e44d30813b8ab0c
manjarid <manjarid@andrew.cmu.edu>
2021-03-19 18:59:25 -0600

    Changed cutoff to perform doubly robust estimation

--- R/psinhat.R
+++ R/psinhat.R
@@ -10,6 +10,7 @@
 #' @param eps The minimum value the estimates can attain to bound them away from zero.
 #' @param iter An integer denoting the maximum number of iterations allowed for targeted maximum likelihood method.
 #' @param sl.lib algorithm library for SuperLearner. Default library includes "gam", "glm", "glmnet", "glm.interaction", "ranger".
+#' @param Nmin The cutoff for minimum sample size to perform doubly robust estimation. Otherwise, Petersen estimator is returned.
 #' @return A list of estimates containing the following components:
 #' \item{psi}{  A dataframe of the estimated capture probability for each list pair, model and method combination. In the absence of covariates, the column represents the standard plug-in estimate.
 #' The rows represent the list pair which is assumed to be independent conditioned on the covariates.
@@ -37,7 +38,7 @@
 #' psin_estimate = psinhat(List_matrix = data, funcname = c("logit", "sl"), nfolds = 2, twolist = FALSE, eps = 0.005)
 #' #this returns the plug-in, the bias-corrected and the tmle estimate for the two models
 #' @export
-psinhat <- function(List_matrix, K = 2, filterrows = TRUE, funcname = c("logit"), nfolds = 5, twolist = FALSE, eps = 0.005, iter = 50, sl.lib = c("SL.gam", "SL.glm", "SL.glm.interaction", "SL.ranger", "SL.glmnet")){
+psinhat <- function(List_matrix, K = 2, filterrows = TRUE, funcname = c("logit"), nfolds = 5, twolist = FALSE, eps = 0.005, iter = 50, sl.lib = c("SL.gam", "SL.glm", "SL.glm.interaction", "SL.ranger", "SL.glmnet"), Nmin = 500){
 
   l = ncol(List_matrix) - K
   n = nrow(List_matrix)
@@ -59,7 +60,7 @@ psinhat <- function(List_matrix, K = 2, filterrows = TRUE, funcname = c("logit")
 
   stopifnot(N > 1)
 
-  if (l >= 0 & N < 500){
+  if (l >= 0 & N < Nmin){
     l = 0
     warning(cat("Insufficient number of observations for doubly-robust estimation."))
   }
